459cb3c8b4b1b63f12abe82ebac5a200dd951bb7
deepakkasera <deepak.kasera143.dk@gmail.com>
2023-11-22 09:26:10 +0530

    Code 22nd Nov

---
+++ src/main/java/org/example/introtooops/Main.java
@@ -0,0 +1,15 @@
+package org.example.introtooops;
+
+public class Main {
+    public static void main(String[] args) {
+        Student obj1 = new Student(); // Object of Student class is created.
+        obj1.name = "Vishnu";
+        obj1.age = 25;
+        obj1.batchName = "Morning batch";
+        obj1.gender = "Male";
+
+        Student obj2 = new Student();
+
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/org/example/introtooops/Student.java
@@ -0,0 +1,17 @@
+package org.example.introtooops;
+
+public class Student {
+    String name;
+    int age;
+    String gender;
+    String batchName;
+
+    //Methods.
+    void changeBatch() {
+        System.out.println("Student is changing their batch");
+    }
+
+    void coursePause() {
+        System.out.println("Student is taking a course pause");
+    }
+}
