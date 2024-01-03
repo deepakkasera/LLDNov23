6685be83f4387aba299fb4cfea6374d1393b1e7b
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-01-03 09:05:26 +0530

    Builder Pattern

---
+++ src/main/java/org/example/designpatterns/builder/Client.java
@@ -0,0 +1,41 @@
+package org.example.designpatterns.builder;
+
+public class Client {
+    public static void main(String[] args) {
+        //Builder builder = new Builder();
+
+//        Builder builder = Student.getBuilder();
+//        builder.setName("Himanshu");
+//        builder.setAge(25);
+//        builder.setBatch("MWF Morning");
+//        builder.setGradYear(2024);
+//
+//        //Student student = new Student(builder);
+//        Student student = builder.build();
+//        System.out.println("DEBUG");
+
+//        Student student1 = new Student(builder)
+
+
+        Student student = Student.getBuilder()
+                .setGradYear(2022)
+                .setName("Himanshu")
+                .setAge(25)
+                .setUnivName("XYZ")
+                .setBatch("MWF Morning")
+                .build();
+
+        System.out.println("DEBUG");
+    }
+}
+
+//Steps:-
+
+/**
+ *
+ * 1. getBuilder() static method
+ * 2. build method in Builder class.
+ *
+ *
+ * Why we made setters return Builder object -> To support Method chaining.
+ */
---
+++ src/main/java/org/example/designpatterns/builder/Student.java
@@ -0,0 +1,162 @@
+package org.example.designpatterns.builder;
+
+import org.example.inheritance3.B;
+
+public class Student {
+    String name;
+    int age;
+    String univName;
+    int gradYear;
+    double psp;
+    String batch;
+    String companyName;
+
+    private Student(Builder builder) {
+        //validations start
+        if (builder.getGradYear() > 2022) {
+            throw new IllegalArgumentException("Grad Year can't be greater than 2022");
+        }
+        //validations end
+        this.name = builder.name;
+        this.age = builder.age;
+        this.batch = builder.batch;
+        this.univName = builder.univName;
+    }
+
+    public static Builder getBuilder() {
+        return new Builder();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUnivName() {
+        return univName;
+    }
+
+    public void setUnivName(String univName) {
+        this.univName = univName;
+    }
+
+    public int getGradYear() {
+        return gradYear;
+    }
+
+    public void setGradYear(int gradYear) {
+        this.gradYear = gradYear;
+    }
+
+    public double getPsp() {
+        return psp;
+    }
+
+    public void setPsp(double psp) {
+        this.psp = psp;
+    }
+
+    public String getBatch() {
+        return batch;
+    }
+
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    static class Builder { // Inner Class
+        String name;
+        int age;
+        String univName;
+        int gradYear;
+        double psp;
+        String batch;
+        String companyName;
+
+        public String getName() {
+            return name;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public String getUnivName() {
+            return univName;
+        }
+
+        public Builder setUnivName(String univName) {
+            this.univName = univName;
+            return this;
+        }
+
+        public int getGradYear() {
+            return gradYear;
+        }
+
+        public Builder setGradYear(int gradYear) {
+            this.gradYear = gradYear;
+            return this;
+        }
+
+        public double getPsp() {
+            return psp;
+        }
+
+        public Builder setPsp(double psp) {
+            this.psp = psp;
+            return this;
+        }
+
+        public String getBatch() {
+            return batch;
+        }
+
+        public Builder setBatch(String batch) {
+            this.batch = batch;
+            return this;
+        }
+
+        public String getCompanyName() {
+            return companyName;
+        }
+
+        public Builder setCompanyName(String companyName) {
+            this.companyName = companyName;
+            return this;
+        }
+
+        public Student build() {
+            return new Student(this);
+        }
+    }
+}
