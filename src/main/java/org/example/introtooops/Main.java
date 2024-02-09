63b0b5fdb4d1d76f5128c73c8003c85d804b7b37
RavitejaBaira <ravitejabaira@gmail.com>
2024-02-09 19:03:11 +0530

    Added something

--- src/main/java/org/example/introtooops/Main.java
+++ src/main/java/org/example/introtooops/Main.java
@@ -2,14 +2,15 @@ package org.example.introtooops;
 
 public class Main {
     public static void main(String[] args) {
-        Student obj1 = new Student(); // Object of Student class is created.
-        obj1.name = "Vishnu";
-        obj1.age = 25;
-        obj1.batchName = "Morning batch";
-        obj1.gender = "Male";
-
-        Student obj2 = new Student();
-
-        System.out.println("Hello");
+//        Student obj1 = new Student(); // Object of Student class is created.
+//        obj1.name = "Vishnu";
+//        obj1.age = 25;
+//        obj1.batchName = "Morning batch";
+//        obj1.gender = "Male";
+//
+//        Student obj2 = new Student();
+//
+//        System.out.println("Hello");
+        System.out.println("Added something for git testing");
     }
 }
